a6866275bba3938c93c0ddd4f98702ed4bd7c293
Hugo Vela <Hugo.Vela@fresnounified.org>
2020-02-09 21:06:34 -0800

    Updating Mongodb URI

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uri = 'mongodb://hugovela1980@gmail.com:small421Feathers/tasks-website-api'
+const uri = 'mongodb://heroku_87rqk2w0:rgpluegdvrg1upq91f0mcg542g@ds047447.mlab.com:47447/heroku_87rqk2w0'
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
